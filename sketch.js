5709585238242b6622eb47ee800389aeb723acf3
vishwachokshi <72859418+vishwachokshi@users.noreply.github.com>
2021-04-09 22:44:21 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var monster,food;
+
+function preload() {
+//preload the images here
+
+}
+
+function setup() {
+  createCanvas(3000, 800);
+  // create sprites here
+
+}
+
+function draw() {
+  background(0);
+createSprite = monster(200,200,20,20);
+createSprite = food (1500,2900,100,10);
+}
+
